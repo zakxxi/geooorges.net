eb4b1d8480ff70fb77a7a92563f4c649fab930fb
Adrien Revel <adrien@abrege.net>
2015-01-14 11:02:38 +0100

    Correcting a small bug on top menu.

--- main.js
+++ main.js
@@ -7,7 +7,20 @@ $.localScroll({offset:-50});
 $('#topCarousel').carousel({
 	interval: 4000,
 	pause: "false"
-
 });
+
+function close_toggle() {
+   if ($(window).width() <= 768) {
+      $('.nav a').on('click', function(){
+          $(".navbar-toggle").click();
+      });
+   }
+   else {
+     $('.nav a').off('click');
+   }
+}
+close_toggle();
+
+$(window).resize(close_toggle);
 
 });
